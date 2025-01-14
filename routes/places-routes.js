70a5f0d0d934277bb50579ce7b41cf318ddd9b8b
Jan Bańczerowski <jasiuu2605@gmail.com>
2025-01-14 20:52:04 +0100

    Manage token logic

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const placesControllers = require("../controllers/places-controller");
-const fileUpload = require('../middleware/file-upload')
+const fileUpload = require("../middleware/file-upload");
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
@@ -10,9 +11,11 @@ router.get("/:pid", placesControllers.getPlaceById);
 
 router.get("/user/:uid", placesControllers.getPlacesByUserId);
 
+router.use(checkAuth);
+
 router.post(
   "/",
-  fileUpload.single('image'),
+  fileUpload.single("image"),
   [
     check("title").not().isEmpty(),
     check("description").isLength({ min: 5 }),
